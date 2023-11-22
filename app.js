5907ae51e04062824f5e41ff01fa763d4e669003
Manuel Casupanan <manuel.casupanan@jmarquardt.com>
2023-11-22 21:12:52 +0100

    pug exercises

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const adminRoutes = require('./routes/admin')
+// set any values globally //sharing data
+app.set('view engine', 'pug') // set templating engine
+app.set('views', 'views') // where to find these template
+
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 // catch all route
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404')
 }) 
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,16 @@ const path = require('path')
 const express = require('express')
 
 const rootDir = require('../util/path')
+const adminData = require('./admin')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    // console.log('shopjs', adminData.products)
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', {prods: products, docTitle: 'Shop'})
+
 }) 
 
 

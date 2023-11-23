e26876b5bd9f7a18341f2864b736ad692a603c35
Manuel Casupanan <manuel.casupanan@jmarquardt.com>
2023-11-23 22:10:26 +0100

    added edit product form

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -8,6 +9,17 @@ exports.getProducts = (req, res, next) => {
       path: '/products'
     });
   });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      pageTitle: 'Product Details',
+      product: product,
+      path: '/products'
+    })
+  })
 };
 
 exports.getIndex = (req, res, next) => {
@@ -26,6 +38,13 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCard = (req,res,next) => {
+  const prodId = req.body.productId
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price)
+  })
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {

390c1cc2f68ab4d7cbdad92b2d90bf5831eaf032
Manuel Casupanan <manuel.casupanan@jmarquardt.com>
2023-11-26 22:19:15 +0100

    upload queries

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-db.execute('SELECT * FROM products')
-
 const errorController = require('./controllers/error')
-const db = require('./util/database')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))

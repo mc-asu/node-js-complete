3248b7876e1343911a15fb64264239861c90885b
Manuel Casupanan <manuel.casupanan@jmarquardt.com>
2023-11-26 23:57:15 +0100

    sequlize introduction

--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql = require('mysql2')
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'rootpassword'
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node-complete', 'root', 'rootpassword', {
+    dialect: 'mysql',
+    host: 'localhost'
 })
 
-module.exports = pool.promise()
+module.exports = sequelize

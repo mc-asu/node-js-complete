028e4cc9603f31c5d3600249363f785258b15a9f
Manuel Casupanan <manuel.casupanan@jmarquardt.com>
2023-11-27 22:47:10 +0100

    Associations and what not

--- controllers/shop.js
+++ controllers/shop.js
@@ -49,6 +49,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId
   let fetchedCart
+  let newQuantity = 1
   req.user.getCart().then(cart => {
     fetchedCart = cart
     return cart.getProducts({where: {id: prodId}})
@@ -57,27 +58,31 @@ exports.postCart = (req, res, next) => {
     if(products.length > 0) {
       product = products[0]
     }
-    let newQuantity = 1
     if(product) {
-      // ...
+      const oldQuantity = product.cartItem.quantity
+      newQuantity = oldQuantity + 1
+      return product
     }
+    
+    return Product.findByPk(prodId)
 
-    return Product.findByPk(prodId).then(product => {
-      return fetchedCart.addProduct(product, { through: { quantity: newQuantity } })
-    }).catch(err => console.log(err))
+  }).then((product) => {
+    return fetchedCart.addProduct(product, { through: { quantity: newQuantity } })
+
+  }).then(() => {
+    res.redirect('/cart');
   })
   .catch(err => console.log(err))
-
-  // Product.findByPk(prodId).then(([product]) => {
-  //   Cart.addProduct(prodId, product[0].price);
-  //   res.redirect('/cart');
-  // })
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId).then(([product]) => {
-    Cart.deleteProduct(prodId, product[0].price);
+  req.user.getCart().then(cart => {
+    return cart.getProducts({where : { id: prodId }})
+  }).then((products) => {
+    const product = products[0]
+    product.cartItem.destroy()
+  }).then((result) => {
     res.redirect('/cart');
   }).catch(err => console.log(err))
 };
